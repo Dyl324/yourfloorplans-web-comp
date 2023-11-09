6829d131c5b63b874a49538d7c815ed5f699dc13
Dyl324 <dyl@yourwebspace.com.au>
2023-11-10 00:17:31 +1100

    Remove export

--- src/web-comp.js
+++ src/web-comp.js
@@ -2,7 +2,7 @@ import { defineCustomElement } from 'vue'
 import VueFloorplanList from './components/FloorplanList.ce.vue'
 // import '../index.css'
 
-export const FloorplanList = defineCustomElement(VueFloorplanList)
+const FloorplanList = defineCustomElement(VueFloorplanList)
 
 customElements.define('floorplan-list', FloorplanList)
 // export function initFloorplans(tagName = 'floorplan-list') {
